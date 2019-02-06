1e75fa4fff4405230751668ea7e5c891652e8655
slavhadz <nufcfen@gmail.com>
2019-02-06 22:25:07 +0200

    updating choosen seets on server

--- cinema-app/cinema-app/src/components/Button/Button.js
+++ cinema-app/cinema-app/src/components/Button/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Button.css';
 
 const button = (props) => (
-    <button className="Button">{props.children}</button>
+    <button className="Button" onClick={props.clicked}>{props.children}</button>
 );
 
 export default button;
--- cinema-app/cinema-app/src/components/Seets/Seets.js
+++ cinema-app/cinema-app/src/components/Seets/Seets.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import Seet from './Seet/Seet';
 import axios from '../../axios';
-import './Seets.css'
+import './Seets.css';
+import Button from '../Button/Button';
 
 class Seets extends Component {
 
@@ -13,29 +14,47 @@ class Seets extends Component {
                 this.setState({cinema: res.data});
             });
         }
-            seetClickedHandler = (number) => {
-                let seetsKey = Object.keys(this.state.cinema)[0];
-                let rows = [...this.state.cinema[seetsKey].rows];
-                rows.map(row => {
-                    row.seets.map(seet => {
-                        if (seet.id === number && seet.changeble) {
-                            seet.ocupied = !seet.ocupied;
-                        }
-                    });
-                })
-                let newSeets = {};
-                newSeets[seetsKey] = rows;
-                this.setState({
-                    ciname: newSeets
-                });
 
-            }
+        seetClickedHandler = (number) => {
+        // let seetsKey = Object.keys(this.state.cinema)[0];
+        let rows = [...this.state.cinema.rows];
+        rows.map(row => {
+            row.seets.map(seet => {
+                if (seet.id === number && seet.changeble) {
+                    seet.ocupied = !seet.ocupied;
+                }
+            });
+        })
+        this.setState({
+            cinema: { rows }
+        });
+    }
+
+    orderClickedHandler = () => {
+        axios.put('/' + this.props.match.params.name + '.json', this.state.cinema)
+            .then(res => console.log('send successfully'));
+        console.log(this.state.cinema);
+    }
 
         render() {
+            let movieName = ''
+            switch(this.props.match.params.name) {
+                case 'batman':
+                    movieName = 'Batman Dark Knight'
+                    break;
+                case 'interstellar':
+                    movieName = 'Interstellar';
+                    break;
+                case 'avengers':
+                    movieName = 'Avengers: Infinity War';
+                    break;
+                default:
+                    movieName = 'Movie name not found';
+            }
             let seets = null;
             if(this.state.cinema) {
-                let seetsKey = Object.keys(this.state.cinema)[0];
-                seets = this.state.cinema[seetsKey].rows.map((row, index) => {
+                // let seetsKey = Object.keys(this.state.cinema)[0];
+                seets = this.state.cinema.rows.map((row, index) => {
                     let rowSeets = row.seets.map(seet => {
                             return ( <Seet key = {seet.id}
                                 clicked = {() => this.seetClickedHandler(seet.id)}
@@ -48,8 +67,16 @@ class Seets extends Component {
 
                 return (
                 <>
-                <div className= 'Seets' > {seets} </div>
-                <div className= 'Screen'>SCREEN</div>
+                <h2 className='textTop'>Choose your seets for <span className='movieName'>{movieName}</span></h2>
+                <div className='container'>
+                    <div className='SeetsContainer'>
+                        <div className= 'Seets' > {seets} </div>
+                        <div className= 'Screen'>SCREEN</div>
+                    </div>
+                    <div className='ButtonContainer'>
+                        <Button clicked={this.orderClickedHandler}>ORDER SEETS</Button>
+                    </div>
+                </div>
                 </>
                 )
             }
